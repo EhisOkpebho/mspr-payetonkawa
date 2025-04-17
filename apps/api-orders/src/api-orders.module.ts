97b57dce88b9783db6aa8cc1626706aa4061d24f
EhisOkpebho <ehisokpebho@users.noreply.github.com>
2025-04-17 11:53:22 +0200

    chore: configure microservices to use their dedicated postgres database hosted on different ports

--- apps/api-orders/src/api-orders.module.ts
+++ apps/api-orders/src/api-orders.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common'
 import { ApiOrdersController } from './api-orders.controller'
 import { ApiOrdersService } from './api-orders.service'
 import { ConfigModule } from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
-		})
+		}),
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: 'localhost',
+			port: 5434,
+			username: 'user',
+			password: 'password',
+			database: 'postgres',
+			autoLoadEntities: true,
+			synchronize: true,
+		}),
 	],
 	controllers: [ApiOrdersController],
 	providers: [ApiOrdersService],
--- apps/api-products/src/api-products.module.ts
+++ apps/api-products/src/api-products.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common'
 import { ApiProductsController } from './api-products.controller'
 import { ApiProductsService } from './api-products.service'
 import { ConfigModule } from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
-		})
+		}),
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: 'localhost',
+			port: 5435,
+			username: 'user',
+			password: 'password',
+			database: 'postgres',
+			autoLoadEntities: true,
+			synchronize: true,
+		}),
 	],
 	controllers: [ApiProductsController],
 	providers: [ApiProductsService],
