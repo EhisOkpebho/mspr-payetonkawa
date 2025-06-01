9763baf97982fdab088fb8fce8c562758f15f342
EhisOkpebho <ehisokpebho@users.noreply.github.com>
2025-06-01 17:29:08 +0200

    feat: secure `api-products.controller.ts` with the auth guard and log user `id` on each request

--- apps/api-products/src/api-products.controller.ts
+++ apps/api-products/src/api-products.controller.ts
@@ -1,7 +1,11 @@
 import { CreateProductDTO, ProductDTO, UpdateProductDTO } from '@app/shared/types/dto/product.dto'
-import { Body, Controller, Delete, Get, Logger, Param, ParseIntPipe, Post, Put } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Logger, Param, ParseIntPipe, Post, Put, UseGuards } from '@nestjs/common'
 import { ApiProductsService } from './api-products.service'
+import { ReqUser } from './_decorators/user.decorator'
+import { User } from '@app/shared/entities/user.entity'
+import { AuthGuard } from './_guards/auth.guard'
 
+@UseGuards(AuthGuard)
 @Controller('products')
 export class ApiProductsController {
 	private readonly logger = new Logger(ApiProductsController.name)
@@ -9,32 +13,32 @@ export class ApiProductsController {
 	constructor(private readonly productsService: ApiProductsService) {}
 
 	@Post()
-	create(@Body() product: CreateProductDTO): Promise<ProductDTO> {
-		this.logger.log('POST /products')
+	create(@Body() product: CreateProductDTO, @ReqUser() user: User): Promise<ProductDTO> {
+		this.logger.log(`POST /products by user ${user.id}`)
 		return this.productsService.create(product)
 	}
 
 	@Put('/:id')
-	update(@Param('id', ParseIntPipe) id: number, @Body() product: UpdateProductDTO): Promise<ProductDTO> {
-		this.logger.log(`PUT /products/${id}`)
+	update(@Param('id', ParseIntPipe) id: number, @Body() product: UpdateProductDTO, @ReqUser() user: User): Promise<ProductDTO> {
+		this.logger.log(`PUT /products/${id} by user ${user.id}`)
 		return this.productsService.update(id, product)
 	}
 
 	@Delete('/:id')
-	delete(@Param('id', ParseIntPipe) id: number): Promise<boolean> {
-		this.logger.log(`DELETE /products/${id}`)
+	delete(@Param('id', ParseIntPipe) id: number, @ReqUser() user: User): Promise<boolean> {
+		this.logger.log(`DELETE /products/${id} by user ${user.id}`)
 		return this.productsService.delete(id)
 	}
 
 	@Get()
-	findAll(): Promise<ProductDTO[]> {
-		this.logger.log('GET /products')
+	findAll(@ReqUser() user: User): Promise<ProductDTO[]> {
+		this.logger.log(`GET /products by user ${user.id}`)
 		return this.productsService.findAll()
 	}
 
 	@Get('/:id')
-	findById(@Param('id', ParseIntPipe) id: number): Promise<ProductDTO> {
-		this.logger.log(`GET /products/${id}`)
+	findById(@Param('id', ParseIntPipe) id: number, @ReqUser() user: User): Promise<ProductDTO> {
+		this.logger.log(`GET /products/${id} by user ${user.id}`)
 		return this.productsService.findById(id)
 	}
 }
